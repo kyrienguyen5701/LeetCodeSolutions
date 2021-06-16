b7637d5654ad420ad14083879a10f5633a3b555f
Kyrie Nguyen <sonnhthanglong@gmail.com>
2021-06-16 02:54:00 -0400

    Matchsticks to Square solved

--- Medium/MatchsticksToSquare.py
+++ Medium/MatchsticksToSquare.py
@@ -8,18 +8,36 @@ Your output will either be true or false, to represent
 whether you could make one square using all the matchsticks the little match girl has.
 '''
 
+def makesquare(matchsticks):
+    n = len(matchsticks)
+    side, rem = divmod(sum(matchsticks) , 4)
+    if rem != 0:
+        return False
+    matchsticks.sort(reverse=True)
+    done = set()
 
-def makesquare(nums):
-    # size = sum(nums) / 4
-    # if size - int(size) != 0:
-    #     return False
-    # size = int(size)
-    # return size
-    return
-
-# print(makesquare([1,1,1,2]))
+    def dfs(i, need):
+        if i == n:
+            return False
+        if i in done:
+            return dfs(i + 1, need)
+        if matchsticks[i] == need:
+            done.add(i)
+            return True
+        if matchsticks[i] < need:
+            done.add(i)
+            if dfs(i + 1, need - matchsticks[i]):
+                return True
+            done.remove(i)
+            return dfs(i + 1, need)
+        return dfs(i + 1, need)
+    
+    for _ in range(4):
+        if not dfs(0, side):
+            return False
+    return True
 
 '''
-Runtime:
-Memory:
+Runtime: 36ms - 98.61%
+Memory: 14.2MB - 90.03%
 '''
